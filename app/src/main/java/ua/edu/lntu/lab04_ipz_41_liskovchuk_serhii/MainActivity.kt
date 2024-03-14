95b64d03dde1ae9c77661f09b047e7c662159a57
mickey394 <tliskovchuk@gmail.com>
2024-03-14 14:21:18 +0200

    Додав Composable-функцію для відображення списку завдань

--- app/src/main/java/ua/edu/lntu/lab04_ipz_41_liskovchuk_serhii/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/lab04_ipz_41_liskovchuk_serhii/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.Text // Додано імпорт
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Preview // Додано імпорт
 import ua.edu.lntu.lab04_ipz_41_liskovchuk_serhii.ui.theme.Lab04_IPZ_41_Liskovchuk_SerhiiTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,15 +18,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04_IPZ_41_Liskovchuk_SerhiiTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    TaskList()
                 }
             }
         }
     }
+}
+
+@Composable
+fun TaskList() {
+
+    Greeting(name = "Android")
 }
 
 @Composable
