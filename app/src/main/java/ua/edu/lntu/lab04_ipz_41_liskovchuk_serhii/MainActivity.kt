48ecad3749b63579993c0492cdfd7cd9aa6e4850
mickey394 <tliskovchuk@gmail.com>
2024-03-14 14:26:49 +0200

    Змінив функцію TaskList відобразив список завдань за допомогою LazyColumn

--- app/src/main/java/ua/edu/lntu/lab04_ipz_41_liskovchuk_serhii/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/lab04_ipz_41_liskovchuk_serhii/MainActivity.kt
@@ -6,18 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text // Додано імпорт
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview // Додано імпорт
+import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.lab04_ipz_41_liskovchuk_serhii.ui.theme.Lab04_IPZ_41_Liskovchuk_SerhiiTheme
+import androidx.compose.material3.Text // Додано імпорт
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Lab04_IPZ_41_Liskovchuk_SerhiiTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     TaskList()
                 }
@@ -28,8 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskList() {
+    val tasks = listOf(
+      "1"
+    )
 
-    Greeting(name = "Android")
+    LazyColumn {
+        items(tasks) { task ->
+            Greeting(name = task)
+        }
+    }
 }
 
 @Composable
