808a5e8d9c6426ec6b8ba2c9c6835a975b009778
mickey394 <tliskovchuk@gmail.com>
2024-03-14 13:25:24 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lab04_IPZ_41_Liskovchuk_Serhii"
+include(":app")
+ 
